dc6bd9cd0df42c7bf6da5f412fc738d2b498dd18
lieftsatyamsrivastava <satyamsrivastava384@gmail.com>
2023-10-31 15:40:51 +0530

    firebase

--- src/components/EmailModal.js
+++ src/components/EmailModal.js
@@ -82,33 +82,293 @@
 
 // export default EmailModal;
 
+// code i have changed 
+
+// import React, { useState } from "react";
+// import { Form, Button, Modal, Alert } from "react-bootstrap";
+// import EmailFormNew from "./EmailModalUpdated";
+// import {AddDoc, Collection} from "firebase/analytics"
+// import {getAnalytics} from "../firebase"
+// import { collection } from "firebase/firestore";
+
+// function EmailModal() {
+//   const [show, setShow] = useState(false);
+//   const [isClicked, setIsClicked] = useState(false);
+//   const [email, setEmail] = useState("");
+//   const [hasError, setHasError] = useState(false);
+
+//   const handleEmailChange = (e) => {
+//     setEmail(e.target.value);
+//     setHasError(false); // Reset error when user types
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (!email) {
+//       setHasError(true);
+//       return;
+//     }
+
+//     // Add your logic for handling the form submission with the email
+//     console.log("Submitted email:", email);
+       
+//     // Close the modal after successful submission
+//     handleClose();
+//   };
+ 
+//   const handleShow = () => setShow(true);
+//   const handleClose = () => {
+//     setShow(false);
+//     setIsClicked(!isClicked);
+//     setHasError(false); // Reset error when modal is closed
+//   };
+//   await AddDoc(collection)(getAnalytics,"email")
+
+
+//   return (
+//     <>
+//       <Button
+//         onClick={handleShow}
+//         style={{ background: "none", color: "#084aa6", border: "none" }}
+//         variant={isClicked ? "white" : "#f5f5f5"}
+//       >
+//         Pitch Deck
+//       </Button>
+
+//       <Modal show={show} onHide={handleClose} centered>
+//         <Modal.Header closeButton>
+//           <Modal.Title style={{ color: "#084aa6" }}>Pitch Deck</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body className="pt-3 pb-5">
+//           {/* <Form onSubmit={handleSubmit}>
+//             <Form.Label>Email address</Form.Label>
+//             <div className="d-flex align-items-center">
+//               <Form.Control
+//                 type="email"
+//                 placeholder="Enter email"
+//                 value={email}
+//                 onChange={handleEmailChange}
+//                 isInvalid={hasError} // Apply Bootstrap's isInvalid prop to show error style
+//               />
+//               <Button
+//                 style={{
+//                   backgroundColor: "#084aa6",
+//                   color: "white",
+//                   borderRadius: "0",
+//                   borderTopRightRadius: "15px",
+//                   borderBottomRightRadius: "15px",
+//                   width:"20%"
+//                 }}
+//                 type="submit"
+//               >
+//                 Submit
+//               </Button>
+//             </div>
+//             {hasError && (
+//               <Alert variant="danger" className="mt-2">
+//                 Email is required.
+//               </Alert>
+//             )}
+//           </Form> */}
+//           <EmailFormNew/>
+//         </Modal.Body>
+//       </Modal>
+//     </>
+//   );
+// }
+
+// export default EmailModal;
+
+//  code changed by satyam 2 time    
+
+// import React, { useState } from "react";
+// import { Form, Button, Modal, Alert } from "react-bootstrap";
+// import { getAnalytics } from "firebase/analytics";
+// import { collection, addDoc, getFirestore } from "firebase/firestore"; // Import getFirestore
+// import { initializeApp } from "firebase/app";
+// import {app} from "../firebase"
+// // const firebaseConfig = {
+// //   apiKey: "AIzaSyCCTJ2spyzXQSxUTe8n4zlh-NimPvg4ho8",
+// //   authDomain: "mypickup-aad9c.firebaseapp.com",
+// //   projectId: "mypickup-aad9c",
+// //   storageBucket: "mypickup-aad9c.appspot.com",
+// //   messagingSenderId: "282092225148",
+// //   appId: "1:282092225148:web:e6270cc2886b212841bff9",
+// //   measurementId: "G-CDLQSPH99X"
+// // };
+
+// // const app = initializeApp(firebaseConfig);
+
+// const EmailModal = () => {
+//   const [show, setShow] = useState(false);
+//   const [isClicked, setIsClicked] = useState(false);
+//   const [email, setEmail] = useState("");
+//   const [hasError, setHasError] = useState(false);
+
+//   const handleEmailChange = (e) => {
+//     setEmail(e.target.value);
+//     setHasError(false); // Reset error when the user types
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (!email) {
+//       setHasError(true);
+//       return;
+//     }
+
+//     // Add your logic for handling the form submission with the email
+//     console.log("Submitted email:", email);
+
+//     // Send email to Firebase collection
+//     const db = getFirestore(app); // Get a Firestore reference
+//     const emailCollection = collection(db, "emails");
+//     addDoc(emailCollection, { email: email });
+
+//     // Close the modal after successful submission
+//     handleClose();
+//   };
+
+//   const handleShow = () => setShow(true);
+//   const handleClose = () => {
+//     setShow(false);
+//     setIsClicked(!isClicked);
+//     setHasError(false); // Reset error when the modal is closed
+//   };
+
+//   const submitData = async(event) =>{
+//     event.preventDefault();
+//     const{email,}= userData;
+//     fetch("https://mypickup-aad9c-default-rtdb.firebaseio.com/userDataEmaul.json",
+//     {
+//       method: "POST",
+//         headers:{
+//           "Content-Type":"application/json"
+
+//         },
+//         body:JSON.stringify({
+//           email
+//         })
+//       }
+//     );
+        
+        
+  
+  
+//   }
+//   return (
+//     <>
+//       <Button
+//         onClick={handleShow}
+//         style={{ background: "none", color: "#084aa6", border: "none" }}
+//         variant={isClicked ? "white" : "#f5f5f5"}
+//       >
+//         Pitch Deck
+//       </Button>
+
+//       <Modal show={show} onHide={handleClose} centered>
+//         <Modal.Header closeButton>
+//           <Modal.Title style={{ color: "#084aa6" }}>Pitch Deck</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body className="pt-3 pb-5">
+//           <Form onSubmit={handleSubmit}>
+//             <Form.Label>Email address</Form.Label>
+//             <div className="d-flex align-items-center">
+//               <Form.Control
+//                 type="email"
+//                 placeholder="Enter email"
+//                 value={email}
+//                 onChange={handleEmailChange}
+//                 isInvalid={hasError} // Apply Bootstrap's isInvalid prop to show error style
+//               />
+//               <Button
+//                 style={{
+//                   backgroundColor: "#084aa6",
+//                   color: "white",
+//                   borderRadius: "0",
+//                   borderTopRightRadius: "15px",
+//                   borderBottomRightRadius: "15px",
+//                   width: "20%"
+//                 }}
+//                 type="submit"
+//               >
+//                 Submit
+//               </Button>
+//             </div>
+//             {hasError && (
+//               <Alert variant="danger" className="mt-2">
+//                 Email is required.
+//               </Alert>
+//             )}
+//           </Form>
+//         </Modal.Body>
+//       </Modal>
+//     </>
+//   );
+// };
+
+// export default EmailModal;
 
 
 import React, { useState } from "react";
 import { Form, Button, Modal, Alert } from "react-bootstrap";
-import EmailFormNew from "./EmailModalUpdated";
+// extra code starts
+// import installations from 'firebase/installations';
+// extra code ends
+import { collection, addDoc, getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push } from "firebase/database"; // Import Realtime Database functions
+import {app} from "../firebase";
 
-function EmailModal() {
+const EmailModal = () => {
   const [show, setShow] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
   const [email, setEmail] = useState("");
   const [hasError, setHasError] = useState(false);
+// // extra code starts
+// const [installationId, setInstallationId] = useState('');
 
+//   useEffect(() => {
+//     const installations = installations();
+//     installations.enableRestrictedApi('Firebase Installations API');
+
+//     installations.createInstallation().then((installation) => {
+//       setInstallationId(installation.installationId);
+//     });
+//   }, []);
+//   console.log("instaLLATION iD", installationId);
+// // extra code ends
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
-    setHasError(false); // Reset error when user types
+    setHasError(false); // Reset error when the user types
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (!email) {
-      setHasError(true);
+      setHasError(true); // error
       return;
     }
 
     // Add your logic for handling the form submission with the email
     console.log("Submitted email:", email);
+
+    // Send email to Firebase Firestore
+    const dbFirestore = getFirestore(app); // Get a Firestore reference
+    const emailCollection = collection(dbFirestore, "emails");
+    // addDoc(emailCollection, { email: email });
+    const newDoc = addDoc(emailCollection, { email: email });
+    console.log(newDoc.id);
+
+    // Send email to Firebase Realtime Database
+    const dbRealtime = getDatabase(app); // Get Realtime Database reference
+    const realtimeRef = ref(dbRealtime, "userDataEmail"); // Specify the path you want to use
+    
+    
+    push(realtimeRef, email);
 
     // Close the modal after successful submission
     handleClose();
@@ -118,7 +378,7 @@ function EmailModal() {
   const handleClose = () => {
     setShow(false);
     setIsClicked(!isClicked);
-    setHasError(false); // Reset error when modal is closed
+    setHasError(false); // Reset error when the modal is closed
   };
 
   return (
@@ -136,7 +396,7 @@ function EmailModal() {
           <Modal.Title style={{ color: "#084aa6" }}>Pitch Deck</Modal.Title>
         </Modal.Header>
         <Modal.Body className="pt-3 pb-5">
-          {/* <Form onSubmit={handleSubmit}>
+          <Form onSubmit={handleSubmit}>
             <Form.Label>Email address</Form.Label>
             <div className="d-flex align-items-center">
               <Form.Control
@@ -153,7 +413,7 @@ function EmailModal() {
                   borderRadius: "0",
                   borderTopRightRadius: "15px",
                   borderBottomRightRadius: "15px",
-                  width:"20%"
+                  width: "20%"
                 }}
                 type="submit"
               >
@@ -165,12 +425,11 @@ function EmailModal() {
                 Email is required.
               </Alert>
             )}
-          </Form> */}
-          <EmailFormNew/>
+          </Form>
         </Modal.Body>
       </Modal>
     </>
   );
-}
+};
 
 export default EmailModal;
